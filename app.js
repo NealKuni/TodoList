054e043be439f687156a6341e6b7fa9e7dbc8559
nealichinohe <neal.kuni@yahoo.com>
2022-04-09 15:44:58 -0600

    added filter

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('filter-todo');
 
 // Event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -53,3 +55,20 @@ function deleteCheck(event){
     }
 }
 
+function filterTodo(e){
+    const todos = todoList.childNodes
+    console.log(todos)
+    todos.forEach(function(todo) {
+        switch(e.target.value){
+            case "all":
+                break;
+            case "completed":
+                if(todo.classList.contains("complete")){
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none"
+                }
+        }
+    }
+}
+ 
