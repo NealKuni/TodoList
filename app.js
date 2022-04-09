e301d1200b978710370a7662c8492ad3110edecc
nealichinohe <neal.kuni@yahoo.com>
2022-04-08 22:39:14 -0600

    more css

--- app.js
+++ app.js
@@ -40,7 +40,11 @@ function deleteCheck(event){
     // DELETE TODO
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        // Animation
+        todo.classList.add('fall')
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
     }
 
     if (item.classList[0]==="complete-btn") {
